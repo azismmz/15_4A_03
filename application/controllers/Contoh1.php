5f71a9ef323cd9bac3332fe19c1ee400111a9b61
azismmz <mochamad.mmz@bsi.ac.id>
2024-04-01 19:38:27 +0700

    Pertemuan 1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,15 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Imam Nawawi Saya tingga di daerah Ciputat olah raga yang saya sukai adalah Bulutangkis";
+    }
+
+    public function tes()
+    {
+        echo "<h1>Warning !!!</h1>";
+        echo "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid neque corporis, earum unde tempore sit magnam voluptatibus quo nulla possimus atque deserunt quam dolorem impedit. Voluptatibus, distinctio impedit! Cumque, deserunt?";
+    }
+}
